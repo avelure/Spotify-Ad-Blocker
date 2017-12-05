ed7eb37f3a02a4a3c7650a34c680f1257ca3f252
avelure <avelure@cern.ch>
2017-12-05 14:32:51 +0100

    Fixed incorrect merge

--- EZBlocker/EZBlocker/WebHelperHook.cs
+++ EZBlocker/EZBlocker/WebHelperHook.cs
@@ -11,6 +11,8 @@ namespace EZBlocker
 {
     class WebHelperHook
     {
+        private const string ua = @"Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; WOW64; Trident/4.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; .NET4.0C; .NET4.0E)";
+        private const string port = ":4371";
         private const string port_old = ":4380";
 
         private static string oauthToken;
